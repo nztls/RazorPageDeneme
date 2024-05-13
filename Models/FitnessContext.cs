d016df8a719fee6316442b10bfc7e628c5f45329
NazT <naztls08@gmail.com>
2024-05-13 19:11:29 +0300

    first commit

---
+++ Models/FitnessContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorPageDeneme.Models;
+
+public partial class FitnessContext : DbContext
+{
+    public FitnessContext()
+    {
+    }
+
+    public FitnessContext(DbContextOptions<FitnessContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var builder = WebApplication.CreateBuilder();
+        var connectionString = builder.Configuration.GetConnectionString ("DefaultConnection");
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
